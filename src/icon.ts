556a45b8b6142e1c7518993cb67c456e4704a790
OPZ <151683640+OPZ-Wallet@users.noreply.github.com>
2024-02-18 16:08:49 +0100

    Update icon.ts

--- src/icon.ts
+++ src/icon.ts
@@ -2,25 +2,25 @@ export default `
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 1080 1080" style="enable-background:new 0 0 1080 1080;" xml:space="preserve">
 <style type="text/css">
-	.st0{fill:#131313;stroke:#000000;stroke-width:1.1511;stroke-miterlimit:10;}
-	.st1{fill-rule:evenodd;clip-rule:evenodd;fill:#8430FC;}
-	.st2{fill-rule:evenodd;clip-rule:evenodd;fill:#7303FC;}
-	.st3{fill-rule:evenodd;clip-rule:evenodd;fill:#5C02CA;}
-	.st4{fill:#7303FC;}
+	.st00{fill:#131313;stroke:#000000;stroke-width:1.1511;stroke-miterlimit:10;}
+	.st01{fill-rule:evenodd;clip-rule:evenodd;fill:#8430FC;}
+	.st02{fill-rule:evenodd;clip-rule:evenodd;fill:#7303FC;}
+	.st03{fill-rule:evenodd;clip-rule:evenodd;fill:#5C02CA;}
+	.st04{fill:#7303FC;}
 </style>
 <g id="XMLID_00000178169072067517276140000008419492617012852630_">
-	<path id="XMLID_00000034067701802432992160000006854786589156000175_" class="st0" d="M869.2,1079.2H210.8c-116,0-210-94-210-210
+	<path id="XMLID_00000034067701802432992160000006854786589156000175_" class="st00" d="M869.2,1079.2H210.8c-116,0-210-94-210-210
 		V210.8c0-116,94-210,210-210h658.4c116,0,210,94,210,210v658.4C1079.2,985.2,985.2,1079.2,869.2,1079.2z"/>
-	<polygon id="XMLID_00000126324328298493318050000003634198904705462656_" class="st1" points="139.6,544.1 341.8,544.1 
+	<polygon id="XMLID_00000126324328298493318050000003634198904705462656_" class="st01" points="139.6,544.1 341.8,544.1 
 		641.1,843.4 439,843.4 	"/>
-	<polygon id="XMLID_00000054971710720330992160000014220815734697762999_" class="st2" points="940.3,544.1 738.1,544.1 
+	<polygon id="XMLID_00000054971710720330992160000014220815734697762999_" class="st02" points="940.3,544.1 738.1,544.1 
 		588.4,693.4 790.6,693.4 	"/>
-	<polygon id="XMLID_00000089564741431117686050000016652827462057169058_" class="st2" points="491.5,386.8 289.3,386.8 139.6,536 
+	<polygon id="XMLID_00000089564741431117686050000016652827462057169058_" class="st02" points="491.5,386.8 289.3,386.8 139.6,536 
 		341.8,536 	"/>
-	<polygon id="XMLID_00000132807848600322054170000016668253150862821025_" class="st3" points="438.8,236.8 641,236.8 940.5,536 
+	<polygon id="XMLID_00000132807848600322054170000016668253150862821025_" class="st03" points="438.8,236.8 641,236.8 940.5,536 
 		738.1,536 	"/>
 	
-		<rect id="XMLID_00000093856149413409397600000006598707471598936971_" x="465.9" y="405.4" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -223.7562 540.0634)" class="st4" width="148.3" height="269.4"/>
+		<rect id="XMLID_00000093856149413409397600000006598707471598936971_" x="465.9" y="405.4" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -223.7562 540.0634)" class="st04" width="148.3" height="269.4"/>
 </g>
 </svg>
 `;
