320c8cbfc468c23f400033463890455a3b387036
OPZ <151683640+OPZ-Wallet@users.noreply.github.com>
2024-02-18 15:39:45 +0100

    Delete directory

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { WalletInit } from '@web3-onboard/common';
-declare function opz(): WalletInit;
-export default opz;
--- dist/types.d.ts
+++
@@ -1,7 +0,0 @@
-import { EIP1193Provider } from '@web3-onboard/common';
-export interface CustomWindow extends Window {
-    ethereum: EIP1193Provider & {
-        isOPZ?: boolean;
-    };
-    $opz: EIP1193Provider;
-}
